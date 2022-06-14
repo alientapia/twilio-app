f03e8d1085d1dbaf69c8e6ad948e3e51e53e3993
Alien Tapia Salvador <alientapia33@gmail.com>
2022-06-13 23:37:38 -0400

    add config file

---
+++ config/config.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+
+const config = {
+  accountSid: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
+  port: process.env.PORT || 3000,
+  dbUrl: process.env.DATABASE_URL,
+};
+
+module.exports = { config };
